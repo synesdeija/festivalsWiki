63a3630a1d5a5631be5a43803c785ba41967ac52
synesdeija <thedeija@gmail.com>
2022-09-25 15:38:06 -0500

    updated verbiage from Posts to Events throughout codebase

---
+++ routes/events.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const eventsController = require("../controllers/events");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+router.get("/:id", ensureAuth, eventsController.getEvent);
+
+router.post("/createEvent", upload.single("file"), eventsController.createEvent);
+
+router.put("/likeEvent/:id", eventsController.likeEvent);
+
+router.delete("/deleteEvent/:id", eventsController.deleteEvent);
+
+module.exports = router;
