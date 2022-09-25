253346953262649ac93d75f26e22f9505aa7f4bd
Aloutfi <63522731+aloutfi@users.noreply.github.com>
2022-09-25 18:02:56 -0500

    fixed logout method

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,14 +45,12 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+  req.logout(function(err) {
+    if (err) {  console.log(err)} 
+
+    req.session.destroy(function(err) {
+      if (err) { console.log(err)} });
+    res.redirect('/');
   });
 };
 
