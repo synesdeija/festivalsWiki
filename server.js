e028cc1db1fe1a1cb742d24ddf2a9c5444189e12
synesdeija <thedeija@gmail.com>
2022-09-25 15:54:08 -0500

    corrected spelling

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/event", eventtRoutes);
+app.use("/event", eventRoutes);
 app.use("/comment", commentRoutes);
 
 //Server Running
