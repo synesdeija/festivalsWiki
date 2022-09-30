5dee51132fe769728d8788fc4b95aba5182d7e6e
Aloutfi <63522731+aloutfi@users.noreply.github.com>
2022-09-30 16:42:18 -0500

    removed event controller from main.js

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const eventsController = require("../controllers/addEvent");
 const profileController=require("../controllers/profile");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
@@ -15,6 +14,5 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
-router.get("/createEvent", ensureAuth, eventsController.createEvent)
 
 module.exports = router;
