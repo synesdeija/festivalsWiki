b2f5504bd0ebbd3cc0db07edbda56721a3768fde
Aloutfi <63522731+aloutfi@users.noreply.github.com>
2022-09-30 16:40:49 -0500

    pointed event routes to eventsController

--- routes/events.js
+++ routes/events.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const eventsController = require("../controllers/addEvent");
-const profileController= require("../controllers/profile")
+const eventsController = require("../controllers/event");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
-router.get("/:id", ensureAuth, profileController.getEvent);
+// router.get("/:id", ensureAuth, eventsController.getEvent);
+router.get("/createEvent", eventsController.getCreateEventPage);
 
 router.post(
   "/createEvent",
@@ -14,8 +14,8 @@ router.post(
   eventsController.createEvent
 );
 
-router.put("/likeEvent/:id", profileController.likeEvent);
+router.put("/likeEvent/:id", eventsController.likeEvent);
 
-router.delete("/deleteEvent/:id", profileController.deleteEvent);
+router.delete("/deleteEvent/:id", eventsController.deleteEvent);
 
 module.exports = router;
