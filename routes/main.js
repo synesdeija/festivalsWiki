fd1d0d7a0de99c474ae02b6f6a1d12d4022e0e6f
Aloutfi <63522731+aloutfi@users.noreply.github.com>
2022-09-30 17:02:47 -0500

    removed home.js

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
-const homeController = require("../controllers/home");
 const profileController=require("../controllers/profile");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
-router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, profileController.getProfile);
 router.get("/feed", ensureAuth, profileController.getFeed);
 router.get("/login", authController.getLogin);
